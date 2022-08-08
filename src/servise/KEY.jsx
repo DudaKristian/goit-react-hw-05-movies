722a26294634595875093cc6f59fcae47b6564f1
Christian Duda <89409014+DudaKristian@users.noreply.github.com>
2022-08-08 19:54:45 +0300

    basic config + import KEY

--- src/servise/KEY.jsx
+++ src/servise/KEY.jsx
@@ -1,4 +1,8 @@
 
-const KEY = "28970047-f00815849f7b994c191d0ff1b"
+const KEY = "7b66a9815954757a118f4f7577a8110b"
 
 export default KEY;
+
+
+// Example API Request
+// https://api.themoviedb.org/3/movie/550?api_key=7b66a9815954757a118f4f7577a8110b
